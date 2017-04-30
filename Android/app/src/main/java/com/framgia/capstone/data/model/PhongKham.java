e322e6e86568b42236122eec20d7897a081a6e4f
huy <nguyenhuy95dn@gmail.com>
2017-04-30 11:29:36 +0700

    chonphongkham

---
+++ Android/app/src/main/java/com/framgia/capstone/data/model/PhongKham.java
@@ -0,0 +1,72 @@
+package com.framgia.capstone.data.model;
+
+/**
+ * Created by Age on 4/30/2017.
+ */
+
+public class PhongKham {
+    private int mMaPhongKham;
+    private String mTenPhongKham;
+    private String mMoTa;
+    private String mHinhAnh;
+    private String mDiaChi;
+    private String mSDT;
+
+    public PhongKham(int maPhongKham, String tenPhongKham, String moTa, String hinhAnh,
+            String diaChi, String SDT) {
+        mMaPhongKham = maPhongKham;
+        mTenPhongKham = tenPhongKham;
+        mMoTa = moTa;
+        mHinhAnh = hinhAnh;
+        mDiaChi = diaChi;
+        mSDT = SDT;
+    }
+
+    public int getMaPhongKham() {
+        return mMaPhongKham;
+    }
+
+    public void setMaPhongKham(int maPhongKham) {
+        mMaPhongKham = maPhongKham;
+    }
+
+    public String getTenPhongKham() {
+        return mTenPhongKham;
+    }
+
+    public void setTenPhongKham(String tenPhongKham) {
+        mTenPhongKham = tenPhongKham;
+    }
+
+    public String getMoTa() {
+        return mMoTa;
+    }
+
+    public void setMoTa(String moTa) {
+        mMoTa = moTa;
+    }
+
+    public String getHinhAnh() {
+        return mHinhAnh;
+    }
+
+    public void setHinhAnh(String hinhAnh) {
+        mHinhAnh = hinhAnh;
+    }
+
+    public String getDiaChi() {
+        return mDiaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        mDiaChi = diaChi;
+    }
+
+    public String getSDT() {
+        return mSDT;
+    }
+
+    public void setSDT(String SDT) {
+        mSDT = SDT;
+    }
+}
---
+++ Android/app/src/main/java/com/framgia/capstone/ui/adapter/OnItemPhongKhamClickListener.java
@@ -0,0 +1,11 @@
+package com.framgia.capstone.ui.adapter;
+
+import com.framgia.capstone.data.model.PhongKham;
+
+/**
+ * Created by Age on 4/30/2017.
+ */
+
+public interface OnItemPhongKhamClickListener {
+    public void onItemClick(PhongKham phongKham);
+}
